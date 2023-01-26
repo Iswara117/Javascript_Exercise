67165088ddfac1df81d72e73034326a830bd0871
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2023-01-26 14:09:48 +0700

    loop downward triangle with number

--- January_Exercise/loop/trianglePattern/withNumber.js
+++ January_Exercise/loop/trianglePattern/withNumber.js
@@ -15,35 +15,6 @@ function trianumber(range){
     console.log(str)
 }
 
-trianumber(5)
-
-// Left Triangle Pattern with 3 symbol 
-function triasymbol(range){
-
-let string = "";
-for (let i = 1; i <= range; i++) {
-  let kondisi = 0;  // first value variable kondisi
-  for (let j = 1; j <= i; j++) {
-    if(kondisi == 3){  // check value in kondisi
-        kondisi = 0;  // if value = 3 , will change this value to 0 
-    }
-    else {
-      if(kondisi == 0){
-        string += '*';
-    }
-    else if(kondisi == 1){
-      string += '$';
-  }
-    else {
-        string += "#";
-    }
-    kondisi++  //increment varibale kondisi
-    }
-  }
-  string += "\n";
-}
-console.log(string);
-}
+trianumber(5);
 
 
-triasymbol(10)
