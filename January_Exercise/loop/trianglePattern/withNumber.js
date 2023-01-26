9db1eed9cfe1b8b826deda50ee9d465b22383c1b
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2023-01-26 14:04:32 +0700

    new loop alogarithm

--- January_Exercise/loop/trianglePattern/withNumber.js
+++ January_Exercise/loop/trianglePattern/withNumber.js
@@ -1,25 +1,49 @@
 // belum fix ya
 
+// Downward Triangle Star Pattern
 function trianumber(range){
     // console.log('masuk')
-    for(var i=10;i>=range;i--){
-        var str = "" ;
-        // console.log(i, '' ,'ini data variable i ');
-        // console.log(str)
-        for(var j=10;j>=i;j--){
-          str += '*'+" ";
-        //   console.log(j, '' ,'ini data variable j ');
-        //   console.log(j)
-        for(var k=0;k<range;k++){
-            str += " "+k+" ";
-            // console.log(k, '' ,'ini data variable k ');
-          //   console.log(j)
-          }
+    var str = " " ;
+    for(var i=0;i<range;i++){
+        for(var j=0;j< range - i;j++){
+          str += j;
         }
-        console.log(str);
+        str += '\n';
+        // console.log(str);
         // console.log('keluar')
       }
-      
+    console.log(str)
 }
 
 trianumber(5)
+
+// Left Triangle Pattern with 3 symbol 
+function triasymbol(range){
+
+let string = "";
+for (let i = 1; i <= range; i++) {
+  let kondisi = 0;  // first value variable kondisi
+  for (let j = 1; j <= i; j++) {
+    if(kondisi == 3){  // check value in kondisi
+        kondisi = 0;  // if value = 3 , will change this value to 0 
+    }
+    else {
+      if(kondisi == 0){
+        string += '*';
+    }
+    else if(kondisi == 1){
+      string += '$';
+  }
+    else {
+        string += "#";
+    }
+    kondisi++  //increment varibale kondisi
+    }
+  }
+  string += "\n";
+}
+console.log(string);
+}
+
+
+triasymbol(10)
