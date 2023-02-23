6feeaee15f0dd28ba998c6b0e3bcd69c78a5d360
Anak Agung Gede Iswara Wijaya <anakagunggedeiswarawijaya@gmail.com>
2023-02-23 20:08:27 +0700

    modify: add comment in code shuffle Array

--- January_Exercise/Array/shuffleArray.js
+++ January_Exercise/Array/shuffleArray.js
@@ -8,8 +8,8 @@ function shuffle_Array(arr1){
     let after_Shuffle = [];
     for (let start = 0; start < arr1.length; start++) {
         // console.log(start++, 'data start')
-        const randomPosition = Math.floor((arr1.length - start) * Math.random()) //get random number ( math.floor round down and math random get number random)
-        const randomItem = arr1.splice(randomPosition, 2) // add new array
+        const randomPosition = Math.floor((arr1.length - start) * Math.random()); //get random number ( math.floor round down and math random get number random)
+        const randomItem = arr1.splice(randomPosition, 2); // add new array
         
         after_Shuffle.push(...randomItem)
     }
